56b491f118c27bef58d67ea8a712a812e411c74b
blinklabs-admin <mike@blinklabs.ai>
2024-08-06 18:01:29 +0800

    Update the contract with new feature of Burnable: Add the Burnable feature from OpenZeppelin to the CustomToken contract. Import the ERC20Burnable contract and inherit from it. This will allow token holders to burn their own tokens and authorized accounts to burn tokens from other accounts.

--- hardhat/contracts/CustomToken.sol
+++ hardhat/contracts/CustomToken.sol
@@ -2,38 +2,26 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title CustomToken
- * @dev Implementation of a custom ERC20 token with Pausable functionality.
+ * @dev Implementation of a custom ERC20 token with burning, pausing, and minting capabilities.
  */
-contract CustomToken is ERC20, Pausable, Ownable {
+contract CustomToken is ERC20, ERC20Burnable, Pausable, Ownable {
     uint256 private immutable _maxSupply;
 
     /**
-     * @dev Constructor that sets the name, symbol, and max supply of the token.
+     * @dev Constructor that sets the name, symbol, and maximum supply of the token.
      * @param name_ The name of the token.
      * @param symbol_ The symbol of the token.
      * @param maxSupply_ The maximum supply of the token.
      */
-    constructor(
-        string memory name_,
-        string memory symbol_,
-        uint256 maxSupply_
-    ) ERC20(name_, symbol_) {
+    constructor(string memory name_, string memory symbol_, uint256 maxSupply_) ERC20(name_, symbol_) {
         require(maxSupply_ > 0, "Max supply must be greater than 0");
         _maxSupply = maxSupply_;
-        _mint(msg.sender, maxSupply_);
-    }
-
-    /**
-     * @dev Returns the max supply of the token.
-     * @return The maximum supply of the token.
-     */
-    function maxSupply() public view returns (uint256) {
-        return _maxSupply;
     }
 
     /**
@@ -53,22 +41,38 @@ contract CustomToken is ERC20, Pausable, Ownable {
     }
 
     /**
+     * @dev Mints new tokens to the specified account.
+     * Can only be called by the contract owner.
+     * Ensures that the total supply never exceeds the maximum supply.
+     * @param to The address that will receive the minted tokens.
+     * @param amount The amount of tokens to mint.
+     */
+    function mint(address to, uint256 amount) public onlyOwner {
+        require(totalSupply() + amount <= _maxSupply, "Minting would exceed max supply");
+        _mint(to, amount);
+    }
+
+    /**
+     * @dev Returns the maximum supply of the token.
+     * @return The maximum supply of the token.
+     */
+    function maxSupply() public view returns (uint256) {
+        return _maxSupply;
+    }
+
+    /**
      * @dev Hook that is called before any transfer of tokens.
      * This includes minting and burning.
-     *
-     * Calling conditions:
-     *
-     * - when the contract is not paused.
-     *
-     * @param from address The account transferring tokens
-     * @param to address The account receiving tokens
-     * @param amount uint256 The amount of tokens being transferred
+     * Checks if the contract is paused before allowing transfers.
+     * @param from address The address which you want to send tokens from
+     * @param to address The address which you want to transfer to
+     * @param amount uint256 the amount of tokens to be transferred
      */
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal override whenNotPaused {
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
         super._beforeTokenTransfer(from, to, amount);
     }
 }
